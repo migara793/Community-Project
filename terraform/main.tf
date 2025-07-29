2e9b1da2f09e6af334fedb2162dab71e0f5bb7fb
migara793 <migarasathsara37@gmail.com>
2025-07-29 13:26:47 -0700

    changed workflow and terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,82 +5,80 @@ terraform {
       version = "~> 6.0"
     }
   }
+
   backend "s3" {
-    key = "aws/ec2-deploy/terraform.tfstate"
+    bucket         = "my-tf-state-bucket-migara123 "   # Update with your bucket name
+    key            = "aws/ec2-deploy/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
   }
 }
 
-
-
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
-resource "aws_instance" "servernode" {
-  ami                    = "ami-042b4708b1d05f512"
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.maingroup.id]
-  iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = var.private_key
-    timeout     = "4m"
-  }
-  tags = {
-    "name" = "DeployVM"
-  }
-}
-resource "aws_iam_instance_profile" "ec2-profile" {
-  name = "ec2-profile"
-  role = "EC2-ECR-AUTH"
-}
-resource "aws_security_group" "maingroup" {
-  egress = [
-    {
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    }
-  ]
-  ingress = [
-    {
-      cidr_blocks      = ["0.0.0.0/0", ]
-      description      = ""
-      from_port        = 22
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 22
-    },
-    {
-      cidr_blocks      = ["0.0.0.0/0", ]
-      description      = ""
-      from_port        = 80
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 80
-    }
-  ]
-}
+
 resource "aws_key_pair" "deployer" {
   key_name   = var.key_name
   public_key = var.public_key
 }
-output "instance_public_ip" {
-  value     = aws_instance.servernode.public_ip
-  sensitive = true
+
+resource "aws_security_group" "maingroup" {
+  name        = "main-sg"
+  description = "Allow web and SSH traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_instance_profile" "ec2-profile" {
+  name = "ec2-profile"
+  role = "EC2-ECR-AUTH"  # Must match the role you created
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = "ami-042b4708b1d05f512"  # Ubuntu 22.04
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.maingroup.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install -y docker.io
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo usermod -aG docker ubuntu
+              EOF
+
+  tags = {
+    Name = "FlaskReactAppServer"
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,21 @@
-variable "region" {
-    default = "us-east-1"
-}
-variable "public_key" {
-  
-}
-variable "private_key" {
-  
-}
 variable "key_name" {
-  
+  description = "Name of the SSH key pair"
+  type        = string
+}
+
+variable "public_key" {
+  description = "Public SSH key"
+  type        = string
+}
+
+variable "private_key" {
+  description = "Private SSH key"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
